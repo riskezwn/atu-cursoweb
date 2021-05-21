f363f440e46c0111d0deaee3eecfc5a16ac8cea2
riskezwn <enriquegarciagasc@gmail.com>
2021-05-21 17:15:56 +0200

    corrections/comments control1 comic

--- projects/control1_comics/js/script.js
+++ projects/control1_comics/js/script.js
@@ -1,4 +1,6 @@
 "use strict";
+
+// Constantes para calcular dechas
 const fecha = new Date();
 const fechaTxt = document.querySelector(".fecha");
 const meses = new Array(
@@ -25,6 +27,7 @@ const diasSemana = new Array(
   "Sábado"
 );
 
+// Ejecutar funcion para mostrar fecha al cargar el documento
 document.addEventListener("DOMContentLoaded", function (event) {
   console.log("DOM fully loaded and parsed");
   fechaTxt.innerHTML =
@@ -42,20 +45,25 @@ document.addEventListener("DOMContentLoaded", function (event) {
     fecha.getMinutes();
 });
 
+// Apertura de página desde el index
 function openComic() {
   open("comic.html", "_self");
 }
 
+// Constantes para refernciar elementos del HTML
 const button = document.querySelectorAll(".li");
 const tab = document.querySelectorAll(".tab");
 
+// Se recorre con un forEach todos los elementos li (botones)
 button.forEach((listLi, i) => {
+  // Se añade un click con una función a cada botón
   button[i].addEventListener("click", () => {
+    // Se recorren todas los elementos de nuevo y se elimina la clase "active" de todas los elementos
     button.forEach((listLi, i) => {
       button[i].classList.remove("active");
       tab[i].classList.remove("active");
     });
-    console.log(button[i]);
+    // Se añade al elemento boton especifico y su correspondiente tab o viñeta la clase "active"
     button[i].classList.add("active");
     tab[i].classList.add("active");
   });
