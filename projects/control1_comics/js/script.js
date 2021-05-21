1f6ad9fc4a4c3209e4c86bf16ab6a015c5d288cd
riskezwn <enriquegarciagasc@gmail.com>
2021-05-21 16:45:45 +0200

    add control1 comic

---
+++ projects/control1_comics/js/script.js
@@ -0,0 +1,62 @@
+"use strict";
+const fecha = new Date();
+const fechaTxt = document.querySelector(".fecha");
+const meses = new Array(
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre"
+);
+const diasSemana = new Array(
+  "Domingo",
+  "Lunes",
+  "Martes",
+  "Miércoles",
+  "Jueves",
+  "Viernes",
+  "Sábado"
+);
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  console.log("DOM fully loaded and parsed");
+  fechaTxt.innerHTML =
+    "Hoy es " +
+    diasSemana[fecha.getDay()] +
+    ", " +
+    fecha.getDay() +
+    " de " +
+    meses[fecha.getMonth()] +
+    " de " +
+    fecha.getFullYear() +
+    " | " +
+    fecha.getHours() +
+    ":" +
+    fecha.getMinutes();
+});
+
+function openComic() {
+  open("../comic.html", "_self");
+}
+
+const button = document.querySelectorAll(".li");
+const tab = document.querySelectorAll(".tab");
+
+button.forEach((allLi, i) => {
+  button[i].addEventListener("click", () => {
+    button.forEach((allLi, i) => {
+      button[i].classList.remove("active");
+      tab[i].classList.remove("active");
+    });
+    console.log(button[i]);
+    button[i].classList.add("active");
+    tab[i].classList.add("active");
+  });
+});
